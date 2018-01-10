59474767376f56c25ca071da910d4447762fbf9a
thomsedavi <thomsedavi@yahoo.co.nz>
2018-01-10 23:19:48 +1300

    Generate a score with multiple channels

--- JMJam.py
+++ JMJam.py
@@ -1,6 +1,8 @@
 # JMJam.py
 #
 # Generate music by having user approve or reject random mutations
+# I have this cool keytar with a looping pedal so going to design with that setup in mind.
+# Please note that my level of music theory is Minimum Necessary and I'm working with that
 #
 # David Thomsen @D_J_T_
 
@@ -8,28 +10,36 @@ from music import *
 from random import *
 from time import sleep
 
-pitches = [[C5, E5], [D5, F5], [E5, G5], [F5, A5], [G5, B5], [A5, C6], [B5, D6], [REST]]
+scale = [REST, 0, 2, 4, 5, 7, 9, 11]
 
-pitches1 = []
-durations1 = [SN, SN, DSN, TN, TN, DSN, SN, SN]
+# Need to work on randomly generated durations that equal a whole note or whatever
+def generateRandomPart(instrument, channel, durations, offset):
+   pitches = []
+   for x in range(len(durations)):
+      pitch = scale[randint(0, 7)]
+      pitches.append(pitch if pitch == REST else pitch + offset)
+   phrase = Phrase(0.0)
+   phrase.addNoteList(pitches, durations)
+   part = Part(instrument, channel)
+   part.addPhrase(phrase)
+   return part
 
-for x in range(8):
-    pitches1.append(pitches[randint(0, 7)])
+score = Score("Score", 120.0)
 
-theme = Phrase()
-theme.addNoteList(pitches1, durations1)
+score.addPart(generateRandomPart(ELECTRIC_BASS, 1, [QN, QN, QN, QN], 24))
+score.addPart(generateRandomPart(ELECTRIC_GRAND, 1, [EN, EN, EN, EN, EN, EN, EN, EN], 48))
 
 # Will play smoother on my laptop if I pause for a moment before playing.
 # I guess even computers need to get mentally prepared.
 # Note: My laptop is now dead anyway so this probably isn't a problem any more?
 sleep(1)
 
-Play.midi(theme)
+Play.midi(score)
 
-while (True):
-   theme = Phrase()
-   pitches1[randint(0, 7)] = pitches[randint(0, 7)]
-   theme.addNoteList(pitches1, durations1)
-   sleep(2)
-   Play.midi(theme)
+#while (True):
+#   theme = Phrase()
+#   pitches1[randint(0, 7)] = pitches[randint(0, 7)]
+#   theme.addNoteList(pitches1, durations1)
+#   sleep(2)
+#   Play.midi(theme)
 
