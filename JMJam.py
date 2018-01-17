3e01bf73e506ba557d5a937db87d49ba40bd0e06
thomsedavi <thomsedavi@yahoo.co.nz>
2018-01-17 23:24:55 +1300

    Less randomness, more patterns

--- JMJam.py
+++ JMJam.py
@@ -10,9 +10,24 @@ from music import *
 from random import *
 from time import sleep
 
-scale = [REST, 0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23]
+class ScaleDefinition:
+   def __init__(self, scalePattern, offset):
+      scale = []
+      note = 0
+      index = 0
+      while note <= 127:
+         scale.append(note)
+         note += scalePattern[index]
+         index += 1
+         index %= 7
 
-class Duration:
+      self.scale = scale
+      self.offset = offset
+      
+   def getPitch(self, pitch):
+      return [self.scale[pitch + self.offset]]
+
+class NoteDefinition:
    def __init__(self, position, duration):
       self.position = position
       self.duration = duration
@@ -20,35 +35,47 @@ class Duration:
 class PartDefinition:
    def __init__(self, durationsLength, pitchesLength):
       self.durationIndex = randint(0, durationsLength - 1)
-      self.pitchIndex = randint(0, pitchesLength - 1)
+      self.pitchIndexes = []
+      for x in range(pitchesLength):
+         self.pitchIndexes.append(randint(0, 1))
 
 class Channel:
-   def __init__(self, instrument, channel, offset):
+   def __init__(self, instrument, channel, scale, offset):
       self.instrument = instrument
       self.channel = channel
       self.offset = offset
-      self.durationLists = [[Duration(0, 0.5), Duration(2, 0.5), Duration(4, 0.25), Duration(5, 0.25), Duration(6, 0.5), Duration(8, 1.0), Duration(12, 0.5), Duration(14, 0.5)], [Duration(0, 1.0), Duration(4, 1.0), Duration(8, 1.0), Duration(12, 0.5)]] # will probably need to randomly generate these as well I guess
+      self.noteLists = [[NoteDefinition(0, 0.5), NoteDefinition(2, 0.5), NoteDefinition(4, 0.25), NoteDefinition(5, 0.25), NoteDefinition(6, 0.5), NoteDefinition(8, 1.0), NoteDefinition(12, 0.5), NoteDefinition(14, 0.5)], [NoteDefinition(0, 1.0), NoteDefinition(4, 1.0), NoteDefinition(8, 1.0), NoteDefinition(12, 0.5)], [NoteDefinition(0, 0.25), NoteDefinition(1, 0.25), NoteDefinition(2, 0.5), NoteDefinition(4, 0.5), NoteDefinition(6, 0.5), NoteDefinition(8, 0.25), NoteDefinition(9, 0.25), NoteDefinition(10, 0.5), NoteDefinition(12, 0.5), NoteDefinition(14, 0.5)]] # will probably need to randomly generate these as well I guess
       self.pitchLists = []
       self.parts = []
+      self.scale = ScaleDefinition(scale, offset)
 
-      for x in range(2):
+      # any kind of simple pattern is more musical than pure randomness to start with
+      for x in range(3):
          pitches = []
-         for x in range(16):
-            pitches.append(randint(0, 2))
+         pitch1 = randint(-1, 1)
+         pitch2 = randint(-1, 1)
+         for x in range(8):
+            y = randint(-2, 2)
+            pitches.append(pitch1 + y)
+            pitches.append(pitch2 + y)
          self.pitchLists.append(pitches)
 
-      self.parts.append(PartDefinition(len(self.durationLists), len(self.pitchLists)))
-      self.parts.append(PartDefinition(len(self.durationLists), len(self.pitchLists)))
+      self.parts.append(PartDefinition(len(self.noteLists), len(self.pitchLists)))
+      self.parts.append(PartDefinition(len(self.noteLists), len(self.pitchLists)))
 
    def generatePart(self):
       pitchesList = []
       durationsList = []
       for part in self.parts:
-         durations = self.durationLists[part.durationIndex]
-         for duration in durations:
-            durationsList.append(duration.duration)
-            pitch = scale[self.pitchLists[part.pitchIndex][duration.position]]
-            pitchesList.append(pitch if pitch == REST else pitch + self.offset)
+         notes = self.noteLists[part.durationIndex]
+         for note in notes:
+            durationsList.append(note.duration)
+            pitch = 0
+            for x in range(len(self.pitchLists)):
+               if part.pitchIndexes[x] == 1:
+                   pitch += self.pitchLists[x][note.position]
+      
+            pitchesList.append(self.scale.getPitch(pitch))
       phrase = Phrase(0.0)
       phrase.addNoteList(pitchesList, durationsList)
       part = Part(self.instrument, self.channel)
@@ -57,8 +84,8 @@ class Channel:
 
 channels = []
 
-channels.append(Channel(ELECTRIC_BASS, 0, 24))
-channels.append(Channel(ELECTRIC_GRAND, 1, 48))
+channels.append(Channel(ELECTRIC_BASS, 0, [2, 2, 1, 2, 2, 2, 1], 24))
+channels.append(Channel(ELECTRIC_GRAND, 1, [2, 2, 1, 2, 2, 2, 1], 36))
 
 score = Score("Score", 120.0)
 
